ebbaf1520da7d7dc940076c13349dca465eadeba
mmig01 <mintim01@naver.com>
2023-11-14 15:08:43 +0900

    수정 단계

--- 실버3/프린터큐.cpp
+++ 실버3/프린터큐.cpp
@@ -1,37 +1,55 @@
 #include<iostream>
-typedef struct circleQueue
+class CircleQueue
 {
-    int MAX_SIZE ;
-    int front ;
-    int rear ;
+    private :
+        int MAX_SIZE ;
+        int front ;
+        int rear ;
+        int * iCircleQueue ;
 
-    void clear()
-    {
-        front = rear ;
-    }
-    int size()
-    {
-        return (front - rear + MAX_SIZE) % MAX_SIZE ;
-    } 
+    public :
+        CircleQueue(int size)
+        { 
+            MAX_SIZE = size ; 
+            iCircleQueue = new int[MAX_SIZE] ;
+        } 
+        ~CircleQueue()
+        {
+            delete iCircleQueue ;
+        } 
+        void clear()
+        {
+            front = rear ;
+        }
+        int size()
+        {
+            return (front - rear + MAX_SIZE) % MAX_SIZE ;
+        } 
 
-    bool isEmpty()
-    {
+        bool isEmpty()
+        {
+            if (front == rear) return true ;
+            else return false ;
+        }
+        bool isFull()
+        {
+            int temp = (rear + 1) % MAX_SIZE ;
+            if (front == temp) return true ;
+            else return false ;
+        }
+        void enqueue(int item)
+        {
+            if (!isFull())
+            {
+                rear = (rear + 1) % MAX_SIZE ;
+                iCircleQueue[rear] = item ;
+            }
+        }
+        void dequeue()
+        {
 
-    }
-    bool isFull()
-    {
-
-    }
-    bool enqueue()
-    {
-
-    }
-    bool dequeue()
-    {
-
-    }
-
-} CircleQueue ;
+        }
+} ;
 int main()
 {
     int a ;
