c5cf5b32dab2acf8bea63f1efa92de0bc438213f
mmig01 <mintim01@naver.com>
2023-12-24 17:53:08 +0900

    마인 크래프트 문제 추가

--- 실버2/no_18111.cpp
+++ 실버2/no_18111.cpp
@@ -4,15 +4,8 @@
 #include<climits>
 using namespace std ;
 typedef vector<int> ROW ;
-int main()
+void input_value(vector<ROW> & ground , int N , int M , int B)
 {
-    int N , M , B ;
-    cin >> N >> M >> B ;
-    int max_height = 0 , min_time = INT_MAX ;
-    int time , top ;
-    vector<ROW> ground ;
-    pair<int , int> time_and_height ;
-
     for (int i = 0 ; i < N ; i ++)
     {
         ROW temp ;
@@ -24,43 +17,37 @@ int main()
         }
         ground.push_back(temp) ; 
     }
-    
+}
+pair<int , int> set_pair(vector<ROW> & ground , int N , int M , int B)
+{
+    int time , top ;
+    int max_height = 0 , min_time = INT_MAX ;
+    pair<int , int> time_and_height ;
     for (int k = 0 ; k < 257 ; k ++)
     {
         int total_block = B ;
         top = 256 - k ;
         time = 0 ;
-        bool possible = true ;
         for (int i = 0 ; i < N ; i ++)
         { 
             for (int j = 0 ; j < M ; j ++)
             {
-                int temp = 0 ;
+                int temp ;
                 if (top > ground[i][j])
                 {
                     temp = top - ground[i][j] ;
-                    if (total_block >= temp)
-                    {
-                        total_block = total_block - temp ;
-                    }
-                    else
-                    {
-                        possible = false ;
-                        break ;
-                    }
+                    total_block = total_block - temp ;
+                    time = time + temp ;
                 }
                 else if (top < ground[i][j])
                 {
-                    temp = (ground[i][j] - top) * 2 ;
+                    temp = (ground[i][j] - top) ;
+                    total_block = total_block + temp ;
+                    time = time + temp * 2 ;
                 }
-
-                time = time + temp ;
             }
-            if (possible == false) break ;
         }
-
-        if (possible == false) continue ;
-        else
+        if (total_block >= 0)
         {
             time_and_height = make_pair(time , top) ;
             //cout << time_and_height.first << ", " << time_and_height.second << '\n' ;
@@ -71,6 +58,18 @@ int main()
             }
         }
     }
-    cout << min_time << " " << max_height ; 
+    return make_pair(min_time , max_height) ;
+}
+int main()
+{
+    vector<ROW> ground ;
+    pair<int , int> min_time_and_max_height ;
+    int N , M , B ;
+    cin >> N >> M >> B ;
+    input_value(ground , N , M , B) ;
+    min_time_and_max_height = set_pair(ground , N , M , B) ;
+    
+    cout << min_time_and_max_height.first ;
+    cout << " " << min_time_and_max_height.second ; 
     return 0 ;
 }
