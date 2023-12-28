26bba418ca4e15d64388905c86dd1796030e3afd
mingi <mintim01@naver.com>
2023-12-28 15:31:50 +0900

    ..

---
+++ silver1/no_1074.cpp
@@ -0,0 +1,40 @@
+//Z
+#include<iostream>
+#include<vector>
+#include<cmath>
+using namespace std ;
+int search_Z(pair<int , int> point , int N , int start_count)
+{
+    int count ;
+    if (N == 1)
+    {
+        
+    }
+    if (point.first <= pow(2 , N - 1))
+    {
+        if (point.second <= pow(2 , N - 1))
+        {
+            count = search_Z(point , N - 1 , start_count + 3 * pow(4 , N - 1)) ;
+        }
+        else
+        {
+            count = search_Z(point , N - 1 , start_count + pow(4 , N - 1)) ;
+        }
+    }
+    else
+    {
+        if (point.second <= pow(2 , N - 1))
+        {
+            count = search_Z(point , N - 1 , start_count + (2 * pow(4 , N - 1))) ;
+        }
+        else
+        {
+            count = search_Z(point , N - 1 , start_count) ;
+        }
+    }
+    return count ;
+}
+int main()
+{
+
+}
