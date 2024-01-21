cb333b5b112abd78f94462ca46ff4760bbec7c70
mmig01 <mintim01@naver.com>
2024-01-21 16:12:40 +0900

    Four Squares 문제 코드 추가

--- silver3/tempCodeRunnerFile.cpp
+++ silver3/tempCodeRunnerFile.cpp
@@ -1,114 +1,41 @@
+// Four Squares
 #include<iostream>
-#include<vector>
-#include<algorithm>
 #include<cmath>
+#include<climits>
 using namespace std ;
-vector<int> vPositive(4001) ;
-vector<int> vNegative(4001) ;
-void showMean(vector<int> & vArr , int N)
+int dp[50001] ;
+int Count(int N)
 {
-    int total = 0 ;
-    double result ;
-    for (int i = 0 ; i < N ; i ++)
-    {  
-        total = total + vArr[i] ;
-    }
-    result = (double)total / N ;
-    
-    cout << floor(result + 0.5) << '\n' ;
-}
-void showManyView(vector<int> & vArr , int N)
-{  
-    int iMaxNum = -4000 ;
-    int iMaxCount = vNegative[4000] ;
-    int iCount = 0 ;
-    for (int i = 0 ; i < N ; i ++)
-    {  
-        if (vArr[i] >= 0)
-            vPositive[vArr[i]] ++ ;
-        else
-            vNegative[(- vArr[i])] ++ ;
-    }
+    int result = INT_MAX ;
+    int cur_num , num , count ;
 
-    for (int i = 3999 ; i > 0 ; i --)
-    {  
-        if (vNegative[i] >= 1)
+    for (int i = (int)sqrt(N) ; i >= 1 ; i --)
+    {
+        num = N ;
+        count = 0 ;
+        cur_num = i ;
+        for (int j = cur_num ; j >= 1  ; j --)
         {
-            if (iMaxCount < vNegative[i])
+            while (true)
             {
-                iMaxCount = vNegative[i] ;
-                iMaxNum = -i ;
-            }     
-        }   
-    }
-    for (int i = 0 ; i < 4001 ; i ++)
-    {  
-        if (iMaxCount < vPositive[i])
-        {
-            iMaxCount = vPositive[i] ;
-            iMaxNum = i ;
-        }     
-    }
-    // maxcount is available
-    for (int i = 3999 ; i > 0 ; i --)
-    {  
-        if (vNegative[i] == iMaxCount)
-        {
-            iCount ++ ;
-            if (iCount == 2)
-                iMaxNum = - i ;
+                int diff = num - j * j ;
+                if (diff < 0) break ;
+                else
+                {
+                    num = diff ;
+                    count ++ ;    
+                }
+            }
         }
+        if (result > count && num == 0)
+            result = count ;
     }
-    for (int i = 0 ; i < 4001 ; i ++)
-    {  
-        if (vPositive[i] == iMaxCount)
-        {
-            iCount ++ ;
-            if (iCount == 2)
-                iMaxNum = i ;
-        }
-    }
-    cout << iMaxNum << '\n' ;
-}
-void showRange(vector<int> & vArr , int N)
-{
-    int iMax = vArr[0] ;
-    int iMin = vArr[0] ;
-    for (int i = 0 ; i < N ; i ++)
-    {
-        if (iMax < vArr[i])
-            iMax = vArr[i] ;
-    }
-    for (int i = 0 ; i < N ; i ++)
-    {
-        if (iMin > vArr[i])
-            iMin = vArr[i] ;
-    }
-
-    cout << iMax - iMin << '\n' ;
-}
-void showMiddle(vector<int> & vArr , int N)
-{
-    cout << vArr[N/2] << '\n' ;
+    return result ;
 }
 int main()
 {
     int N ;
-    vector<int> vArr ;
-
     cin >> N ;
-    for (int i = 0 ; i < N ; i ++)
-    {  
-        int temp ;
-        cin >> temp ;
-        vArr.push_back(temp) ;
-    }
-    sort(vArr.begin() , vArr.end()) ;
-
-    showMean(vArr , N) ;
-    showMiddle(vArr , N) ;
-    showManyView(vArr , N) ;
-    showRange(vArr , N) ;
-
+    cout << Count(N) ;
     return 0 ;
 }
